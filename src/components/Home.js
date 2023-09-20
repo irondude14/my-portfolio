4907925a9734226e6e0e8f0aea4712674a85cff5
Vladyslav <vkadyrov@protonmail.com>
2023-09-19 20:42:42 -0400

    Home was updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
+import { Link } from 'react-scroll';
 
 function Home() {
   return (
@@ -18,7 +19,9 @@ function Home() {
         </p>
         <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-red-600 hover:border-red-600'>
-            My Work
+            <Link to='work' smooth={true} duration={500}>
+              My Work
+            </Link>
             <div className='group-hover:rotate-90 group-hover:-translate-y-0.5 duration-300'>
               <HiArrowNarrowRight className='ml-2' />
             </div>
